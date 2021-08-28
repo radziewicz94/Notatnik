ab173b6c67348ff34b74d6679d55d1ba08e8a6b6
Radzi <radziewicz94@gmail.com>
2021-08-28 23:23:51 +0200

    dodanie bazy danych, implementacja SQLiteOpenHelper

---
+++ app/src/main/java/pl/mradziewicz/notatnik/DataBaseHelper.kt
@@ -0,0 +1,32 @@
+package pl.mradziewicz.notatnik
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.provider.BaseColumns
+
+object TableInfo : BaseColumns {
+    const val TABLE_NAME = "My_notes"
+    const val TABLE_COLUMN_TITLE = "title"
+    const val TABLE_COLUMN_MESSAGE = "message"
+}
+
+        object BasicComamnd{
+            const val SQL_CREATE_TABLE =
+                "CREATE TABLE ${TableInfo.TABLE_NAME} (" +
+                        "${TableInfo.TABLE_COLUMN_TITLE} INTEGER PRIMARY KEY," +
+                        "${TableInfo.TABLE_COLUMN_MESSAGE} TEXT)"
+
+            const val SQL_DELETE_TABLE = " DROP TABLE IF EXSISTS ${TableInfo.TABLE_NAME}"
+        }
+class DataBaseHelper(context: Context) : SQLiteOpenHelper(context, TableInfo.TABLE_NAME, null, 1){
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL(BasicComamnd.SQL_CREATE_TABLE)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        db?.execSQL(BasicComamnd.SQL_DELETE_TABLE)
+        onCreate(db)
+    }
+
+}
--- app/src/main/java/pl/mradziewicz/notatnik/MainActivity.kt
+++ app/src/main/java/pl/mradziewicz/notatnik/MainActivity.kt
@@ -7,5 +7,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val dataBaseHelper = DataBaseHelper(applicationContext)
+        val db = dataBaseHelper.writableDatabase
     }
 }
